7809fd6cd7b5412e2683a493b7c8e38a6b3da34d
74805 <yuvtaub1308@gmail.com>
2023-06-03 00:34:49 +0300

    Added Phone methods

--- Exe2/Phone.java
+++ Exe2/Phone.java
@@ -2,6 +2,7 @@ package Exe2;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Phone {
     private List<App> apps;
@@ -14,5 +15,55 @@ public class Phone {
         // apps.add(new SMSApp());
         // apps.add(new CalenderApp());
         // apps.add(new MediaApp());
+    }
+
+    public void run() {
+        boolean exit = false;
+        Scanner scanner = new Scanner(System.in);
+
+        while (!exit) {
+            System.out.println("Phone");
+            System.out.println("1. Contacts");
+            System.out.println("2. SMS");
+            System.out.println("3. Calender");
+            System.out.println("4. Media");
+            System.out.println("5. Exit");
+            System.out.print("Enter your choice: ");
+            int userChoice = scanner.nextInt();
+
+            switch (userChoice) {
+                case 1:
+                    apps.get(0).run();
+                    break;
+                case 2:
+                    apps.get(1).run();
+                    break;
+                case 3:
+                    apps.get(2).run();
+                    break;
+                case 4:
+                    apps.get(3).run();
+                    break;
+                case 5:
+                    exit(scanner);
+                    exit = true;
+                    break;
+                default:
+                    System.out.println("Invalid choice, please try again");
+                    break;
+            }
+        }
+    }
+
+    public void printAll() {
+        System.out.println("Printing all apps");
+        for (App app : apps) {
+            app.printAll();
+        }
+    }
+
+    public void exit(Scanner scanner) {
+        System.out.println("Exiting phone");
+        scanner.close();
     }
 }
