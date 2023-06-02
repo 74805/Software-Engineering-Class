6e9352236266694d300589274a9a19a03dbde4ea
74805 <yuvtaub1308@gmail.com>
2023-06-02 23:18:27 +0300

    Created classes

---
+++ Exe2/MediaApp.java
@@ -0,0 +1,19 @@
+package Exe2;
+
+import java.util.List;
+
+public class MediaApp {
+    private List<Media> media;
+
+    public static abstract class Media {
+        // Media class members and methods
+    }
+
+    public static class Video extends Media {
+        // Video class members and methods
+    }
+
+    public static class Music extends Media {
+        // Music class members and methods
+    }
+}
