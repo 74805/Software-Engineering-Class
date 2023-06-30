dd9a0c5c880e5ef1c7cf05fbcc7ac0a915acffaa
74805 <yuvtaub1308@gmail.com>
2023-07-01 02:20:06 +0300

    Created cell interface functions

--- Exe3/Cell.java
+++ Exe3/Cell.java
@@ -1,5 +1,29 @@
 package Exe3;
 
 public abstract class Cell {
+    private int x;
+    private int y;
+    private boolean alive;
 
+    public Cell(int x, int y, boolean alive) {
+        this.x = x;
+        this.y = y;
+        this.alive = alive;
+    }
+
+    public abstract void display();
+
+    public abstract void update();
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public boolean isAlive() {
+        return alive;
+    }
 }
