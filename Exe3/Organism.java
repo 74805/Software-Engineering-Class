40defbaef216e0e4952b3ba491faafe5f09d3706
74805 <yuvtaub1308@gmail.com>
2023-07-02 02:37:45 +0300

    Iimplemented die and increaseAge functions

--- Exe3/Organism.java
+++ Exe3/Organism.java
@@ -12,22 +12,38 @@ public class Organism {
     // hyperparameter Lifespan Multiplier
     private static final int lifespanMultiplier = 100;
 
+    // age is the amout of ticks that have passed since the organism was created
+    private int age;
+
     public Organism(List<OrganismCell> cells) {
         this.cells = cells;
         this.damage = 0;
+        this.age = 0;
     }
 
+    // When touched by a killer cell, an organism will take damage. Once it has
+    // taken as much damage as it has cells in its body, it will die
     public void takeDamage() {
         damage += 1;
 
-        if (damage >= cells.size() * lifespanMultiplier) {
+        if (damage >= cells.size()) {
+            die();
+        }
+    }
+
+    public void increaseAge() {
+        age += 1;
+
+        if (age >= cells.size() * lifespanMultiplier) {
             die();
         }
     }
 
     private void die() {
         // turn all cells into food cells
-
+        for (OrganismCell cell : cells) {
+            cell.die();
+        }
     }
-
 }
+
