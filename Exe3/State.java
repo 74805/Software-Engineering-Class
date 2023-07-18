6c9543d6cb40fafcbbfaeb71933ce6a1a50b4095
74805 <yuvtaub1308@gmail.com>
2023-07-19 00:40:50 +0300

    added cell types to State

--- Exe3/State.java
+++ Exe3/State.java
@@ -1,11 +1,28 @@
 package Exe3;
 
+import Exe3.Cells.EmptyCell;
+import Exe3.Cells.OrganismCells.KillerCell;
+import Exe3.Cells.OrganismCells.MouthCell;
+import Exe3.Cells.OrganismCells.ProducerCell;
+import Exe3.Cells.FoodCell;
+//TODO: import Exe3.Cells.OrganismCells.MoverCell; 
+
 public enum State {
-    SAME,
-    EMPTY,
-    FOOD,
-    KILLER,
-    MOUTH,
-    PRODUCER,
-    MOVER
+    SAME(null),
+    EMPTY(EmptyCell.class),
+    FOOD(FoodCell.class),
+    KILLER(KillerCell.class),
+    MOUTH(MouthCell.class),
+    PRODUCER(ProducerCell.class);
+    // TODO: MOVER(MoverCells.class);
+
+    private Class<?> associatedClass;
+
+    State(Class<?> associatedClass) {
+        this.associatedClass = associatedClass;
+    }
+
+    public Class<?> getCellType() {
+        return associatedClass;
+    }
 }
